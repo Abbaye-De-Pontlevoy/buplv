856e97ba3197b3068cea0c486cadc14a14eeed7e
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-03-13 22:09:54 +0900

    all permitting middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { updateSession } from "./lib";
 
 export async function middleware(request) {
-  return await updateSession(request);
+  //await updateSession(request);
+  return NextResponse.next();
 }
