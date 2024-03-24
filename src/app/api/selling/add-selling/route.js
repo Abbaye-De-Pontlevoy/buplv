9617ab2a4196e713e561fbeb21cbdb89946299a8
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-03-24 13:41:11 +0900

    remove useless fonction

--- src/app/api/selling/add-selling/route.js
+++ src/app/api/selling/add-selling/route.js
@@ -2,27 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { getUserID } from "@/app/helpers/getUserID";
 
-const getRandomId = () => {
-	return Math.floor(Math.random() * (999999999 - 1000000 + 1)) + 1000000;
-};
-  
-const addEntryWithRandomId = async (data) => {
-	let randomId = getRandomId();
-	// Vérifiez si l'ID est déjà utilisé dans votre table
-	while (await prisma.maTable.findUnique({ where: { id: randomId } })) {
-	  randomId = getRandomId();
-	}
-	// Une fois qu'un ID unique est généré, ajoutez l'entrée dans la table
-	const entry = await prisma.maTable.create({
-	  data: {
-		id: randomId,
-		...data,
-	  },
-	});
-	return entry;
-};
-  
-
 export async function POST(req) {
 	const data = await req.json();
 
