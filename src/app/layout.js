5b577359098560a7a199f10edaff02c6c28bbde8
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-03-25 21:11:47 +0900

    add root id to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" id="root">
       <body>{children}</body>
     </html>
   );
