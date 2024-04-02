2c4eb9b27238bdc8402a633bd23ec412ac9a1939
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-02 17:56:03 +0900

    update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -8,11 +8,13 @@ export async function middleware( request ) {
   if(isConnected) {
     const restrictedPaths = ['/login', '/register'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/dashboard');
       return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/dashboard');
     }
   }else{
     const restrictedPaths = ['/dashboard', '/profil'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/dashboard');
       return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") +  process.env.NEXT_PUBLIC_URL + '/login');
     }
   }
