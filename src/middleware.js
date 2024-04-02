77ade19b717f3a621316ce31d4b20420d9b2d7e4
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-02 15:57:32 +0900

    update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -8,12 +8,12 @@ export async function middleware( request ) {
   if(isConnected) {
     const restrictedPaths = ['/login', '/register'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      return NextResponse.redirect((process.env.VERCEL_ENV === 'production' ? "https://buplv.vercel.com" : process.env.NEXT_PUBLIC_URL) + '/dashboard');
+      return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.com" : process.env.NEXT_PUBLIC_URL) + '/dashboard');
     }
   }else{
     const restrictedPaths = ['/dashboard', '/profil'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      return Response.redirect((process.env.VERCEL_ENV === 'production' ? "https://buplv.vercel.com" : process.env.NEXT_PUBLIC_URL) + '/login');
+      return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.com" : process.env.NEXT_PUBLIC_URL) + '/login');
     }
   }
   
