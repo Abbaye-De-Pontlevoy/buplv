8b835d1433b26de273b3ea2fb5cd5cfb0950967b
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-02 18:18:22 +0900

    correct with NEXT_PUBLIC_VERCEL_URL...

--- src/middleware.js
+++ src/middleware.js
@@ -8,18 +8,18 @@ export async function middleware( request ) {
   if(isConnected) {
     const restrictedPaths = ['/login', '/register'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/dashboard');
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/dashboard');
       console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-      console.log("process.env.NEXT_PUBLIC_URL", process.env.NEXT_PUBLIC_URL);
-      return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/dashboard');
+      console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
+      return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/dashboard');
     }
   }else{
     const restrictedPaths = ['/dashboard', '/profil'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_URL + '/login');
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/login');
       console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-      console.log("process.env.NEXT_PUBLIC_URL", process.env.NEXT_PUBLIC_URL);
-      return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") +  process.env.NEXT_PUBLIC_URL + '/login');
+      console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
+      return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") +  process.env.NEXT_PUBLIC_VERCEL_URL + '/login');
     }
   }
   
