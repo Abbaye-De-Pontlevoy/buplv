7b69dee9647ee2de8799da801e12268cb45307c0
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-02 18:28:04 +0900

    use explicit domain names...

--- src/middleware.js
+++ src/middleware.js
@@ -8,18 +8,14 @@ export async function middleware( request ) {
   if(isConnected) {
     const restrictedPaths = ['/login', '/register'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/dashboard');
-      console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-      console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
-      return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/dashboard');
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.app/" : "http://localhost:3000/") + '/dashboard');
+      return NextResponse.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.app/" : "http://localhost:3000/") + '/dashboard');
     }
   }else{
     const restrictedPaths = ['/dashboard', '/profil'];
     if (restrictedPaths.includes(request.nextUrl.pathname)) {
-      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") + process.env.NEXT_PUBLIC_VERCEL_URL + '/login');
-      console.log("process.env.NEXT_PUBLIC_VERCEL_ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
-      console.log("process.env.NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
-      return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://" : "") +  process.env.NEXT_PUBLIC_VERCEL_URL + '/login');
+      console.log((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.app/" : "http://localhost:3000/") + '/login');
+      return Response.redirect((process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://buplv.vercel.app/" : "http://localhost:3000/") + '/login');
     }
   }
   
