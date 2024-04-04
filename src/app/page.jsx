c6348a0f4de195bdeea06bca7c2ed900e8dbacbf
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-04 22:50:06 +0900

    restore page...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,28 @@ import "./page.css";
 export default async function Page() {
   return (
     <>
-      <Header displayAccountButton={true}/>
+      <Header displayAccountButton={true} />
       <Menu current="/" />
       <div className="bandeau-rangement">
-        <form action="/details" className="buttonForm">
-          <button type="submit" id="participeButton"> Je participe à la bourse !</button>
+        <form action="/dashboard" className="buttonForm">
+          <button type="submit" id="participeButton">
+            {" "}
+            Je participe à la bourse !
+          </button>
         </form>
+      </div>
+      <div id="corpus-div">
+        <h1>La bourse à l'uniforme</h1>
+        <p>
+          La bourse à l'uniforme est un événement organisé par l'association des
+          parents d'élèves de l'abbaye de Pontlevoy. C'est un moment de partage
+          et de solidarité où les familles peuvent donner, échanger ou acheter
+          des uniformes scolaires d'occasion. L'objectif de la bourse à
+          l'uniforme est de favoriser le recyclage des vêtements et de permettre
+          aux familles de réaliser des économies. C'est aussi l'occasion de
+          rencontrer d'autres parents et de tisser des liens au sein de la
+          communauté éducative.
+        </p>
       </div>
     </>
   );
