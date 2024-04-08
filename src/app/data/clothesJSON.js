cdffd71071a5400cefcd0b20d3b75be41bd1aeb4
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-08 19:20:49 +0900

    Update

--- src/app/data/clothesJSON.js
+++ src/app/data/clothesJSON.js
@@ -1,357 +1,666 @@
 export const clothesJSON = {
-  Collège: {
-    Fille: {
-      "B&C": {
-        "Doudoune sans manche": {
-          size: ["XS", "S", "M", "L", "XL", "XXL"],
-          price: 8,
-          apelPart: 1.6,
-        },
-      },
-      "Poléo Prestige": {
-        "Polo blanc": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Poléo Standard": {
-        "Polo blanc": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Polo rose": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Uniforme Prestige": {
-        "Chemisier blanc": {
-          size: ["10", "12", "14", "16", "18", "20", "22", "24", "26", "28"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        "Polo blanc": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "35",
-          ],
-          price: 5,
-          apelPart: 1,
-        },
-        "Polo rose": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "36",
-          ],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "33",
-          ],
-          price: 10,
-          apelPart: 2,
-        },
-        "Pull bleu marine laine": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "32",
-          ],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-    },
-    Garçon: {
-      "B&C": {
-        "Doudoune sans manche": {
-          size: ["XS", "S", "M", "L", "XL", "XXL"],
-          price: 8,
-          apelPart: 1.6,
-        },
-      },
-      //}, <== retirer cette accolade
-      "Poléo Prestige": {
-        "Polo blanc": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Poléo Standard": {
-        "Polo blanc": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Polo bleu": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Uniforme Prestige": {
-        "Chemise blanche": {
-          size: ["10", "12", "14", "16", "18", "20", "22", "24", "26", "28"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        "Polo blanc": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "40",
-          ],
-          price: 5,
-          apelPart: 1,
-        },
-        "Polo bleu": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "41",
-          ],
-          price: 5,
-          apelPart: 1,
-        },
-        "Pull bleu marine coton": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "38",
-          ],
-          price: 10,
-          apelPart: 2,
-        },
-        "Pull bleu marine laine": {
-          size: [
-            "10",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "37",
-          ],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-    },
-  },
-  Lycée: {
-    Fille: {
-      "B&C": {
-        "Doudoune sans manche": {
-          size: ["XS", "S", "M", "L", "XL", "XXL"],
-          price: 8,
-          apelPart: 1.6,
-        },
-      },
-      "Poléo Prestige": {
-        "Chemisier blanc": {
-          size: ["12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        //}, <== retirer cette accolade
-        "Pull bleu marine coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Poléo Standard": {
-        "Chemisier blanc": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        "Pull gris coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Uniforme Prestige": {
-        "Chemisier blanc": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "32"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        "Pull bleu marine coton": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "31"],
-          price: 10,
-          apelPart: 2,
-        },
-        "Pull gris liseré rouge laine": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "30"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-    },
-    Garçon: {
-      "B&C": {
-        "Doudoune sans manche": {
-          size: ["XS", "S", "M", "L", "XL", "XXL"],
-          price: 8,
-          apelPart: 1.6,
-        },
-      },
-      "Poléo Prestige": {
-        "Chemise blanche": {
-          size: ["12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        Cravate: {
-          size: ["taille unique"],
-          price: 4,
-          apelPart: 0.8,
-        },
-        "Pull rouge coton": {
-          size: ["12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Poléo Standard": {
-        "Chemise blanche": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        Cravate: {
-          size: ["taille unique"],
-          price: 4,
-          apelPart: 0.8,
-        },
-        "Pull rouge coton": {
-          size: ["10", "12", "14", "16", "XS", "S", "M", "L", "XL", "XXL"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-      "Uniforme Prestige": {
-        "Chemise blanche": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "35"],
-          price: 7,
-          apelPart: 1.4,
-        },
-        Cravate: {
-          size: ["taille unique"],
-          price: 4,
-          apelPart: 0.8,
-        },
-        "Pull rouge coton": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "34"],
-          price: 10,
-          apelPart: 2,
-        },
-        "Pull rouge laine": {
-          size: ["14", "16", "18", "20", "22", "24", "26", "28", "33"],
-          price: 10,
-          apelPart: 2,
-        },
-      },
-    },
-  },
+	"Collège": {
+		"Fille": {
+			"B&C": {
+				"Doudoune sans manche": {
+					"size": [
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 8,
+					"apelPart": 1.6
+				},
+			},
+			"Poléo Prestige": {
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Poléo Standard": {
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Polo rose": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Uniforme Prestige": {
+				"Chemisier blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"35"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Polo rose": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"36"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"33"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+				"Pull bleu marine laine": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"32"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+		},
+		"Garçon": {
+			"B&C": {
+				"Doudoune sans manche": {
+					"size": [
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 8,
+					"apelPart": 1.6
+				},
+			},
+			"Poléo Prestige": {
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Poléo Standard": {
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Polo bleu": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Uniforme Prestige": {
+				"Chemise blanche": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Polo blanc": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"40"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Polo bleu": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"41"
+					],
+					"price": 5,
+					"apelPart": 1
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"38"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+				"Pull bleu marine laine": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30",
+						"37"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+		},
+	},
+	"Lycée": {
+		"Fille": {
+			"B&C": {
+				"Doudoune sans manche": {
+					"size": [
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 8,
+					"apelPart": 1.6
+				}
+			},
+			"Poléo Prestige": {
+				"Chemisier blanc": {
+					"size": [
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+			},
+			"Pull bleu marine coton": {
+				"size": [
+					"10",
+					"12",
+					"14",
+					"16",
+					"XS",
+					"S",
+					"M",
+					"L",
+					"XL",
+					"XXL"
+				],
+				"price": 10,
+				"apelPart": 2
+			},
+		},
+		"Poléo Standard": {
+			"Chemisier blanc": {
+				"size": [
+					"10",
+					"12",
+					"14",
+					"16",
+					"XS",
+					"S",
+					"M",
+					"L",
+					"XL",
+					"XXL"
+				],
+				"price": 7,
+				"apelPart": 1.4
+			},
+			"Pull gris coton": {
+				"size": [
+					"10",
+					"12",
+					"14",
+					"16",
+					"XS",
+					"S",
+					"M",
+					"L",
+					"XL",
+					"XXL"
+				],
+				"price": 10,
+				"apelPart": 2
+			},
+			"Uniforme Prestige": {
+				"Chemisier blanc": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"32"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Pull bleu marine coton": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"31"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+				"Pull gris liseré rouge laine": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"30"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+		},
+		"Garçon": {
+			"B&C": {
+				"Doudoune sans manche": {
+					"size": [
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 8,
+					"apelPart": 1.6
+				}
+			},
+			"Poléo Prestige": {
+				"Chemise blanche": {
+					"size": [
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Cravate": {
+					"size": [
+						"taille unique"
+					],
+					"price": 4,
+					"apelPart": 0.8
+				},
+				"Pull rouge coton": {
+					"size": [
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Poléo Standard": {
+				"Chemise blanche": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Cravate": {
+					"size": [
+						"taille unique"
+					],
+					"price": 4,
+					"apelPart": 0.8
+				},
+				"Pull rouge coton": {
+					"size": [
+						"10",
+						"12",
+						"14",
+						"16",
+						"XS",
+						"S",
+						"M",
+						"L",
+						"XL",
+						"XXL"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+			"Uniforme Prestige": {
+				"Chemise blanche": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"35"
+					],
+					"price": 7,
+					"apelPart": 1.4
+				},
+				"Cravate": {
+					"size": [
+						"taille unique"
+					],
+					"price": 4,
+					"apelPart": 0.8
+				},
+				"Pull rouge coton": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"34"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+				"Pull rouge laine": {
+					"size": [
+						"14",
+						"16",
+						"18",
+						"20",
+						"22",
+						"24",
+						"26",
+						"28",
+						"33"
+					],
+					"price": 10,
+					"apelPart": 2
+				},
+			},
+		},
+	},
 };
