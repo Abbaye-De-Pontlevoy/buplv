6e491131feb5c5a4e9206f543abd15348fdb8387
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-08 12:06:16 +0900

    add real articles

--- src/app/components/Article/ArticleForm/ArticleForm.jsx
+++ src/app/components/Article/ArticleForm/ArticleForm.jsx
@@ -6,6 +6,8 @@ import "./styles.css";
 
 const ArticleForm = ({ callAfterSubmit, title }) => {
   const articleData = clothesJSON;
+  const [grade, setGrade] = useState("");
+  const [sex, setSex] = useState("");
   const [name, setName] = useState("");
   const [brand, setBrand] = useState("");
   const [size, setSize] = useState("");
@@ -14,12 +16,9 @@ const ArticleForm = ({ callAfterSubmit, title }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setSize("");
-    setBrand("");
   }, [name]);
 
   useEffect(() => {
-    setSize("");
   }, [brand]);
 
   const handleSubmit = async (e) => {
@@ -33,7 +32,7 @@ const ArticleForm = ({ callAfterSubmit, title }) => {
       brand: brand,
       size: size,
       quantity: quantity,
-      price: articleData[name]["price"]
+      price: articleData[grade][sex][brand][name]["price"],
     };
     const result = await addArticle(submitData);
 
@@ -50,97 +49,126 @@ const ArticleForm = ({ callAfterSubmit, title }) => {
       <h3 id="articleFormTitle">{title}</h3>
       <form onSubmit={handleSubmit} id="addArticleForm">
         <table>
-        <tbody>
-        <tr>
-          <td>
-            <select
-              name="name"
-              key="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              disabled={!articleData || isLoading}
-            >
-              <option key="default" value="">
-                Article
-              </option>
-              {Object.keys(articleData).map((articleName) => (
-                <option key={articleName} value={articleName}>
-                  {articleName}
-                </option>
-              ))}
-            </select>
-          </td>
-          <td>
-            <select
-              name="brand"
-              key="brand"
-              value={brand}
-              onChange={(e) => setBrand(e.target.value)}
-              disabled={!name || isLoading}
-            >
-              <option key="default" value="">
-                Marque
-              </option>
-              {name &&
-                articleData[name]["brand"].map((brand) => (
-                  <option key={brand} value={brand}>
-                    {brand}
+          <tbody>
+            <tr>
+              <td>
+                <select
+                  name="grade"
+                  key="grade"
+                  value={grade}
+                  onChange={(e) => setGrade(e.target.value)}
+                  disabled={!articleData || isLoading}
+                >
+                  <option key="default" value="">
+                    Niveau
                   </option>
-                ))}
-            </select>
-          </td>
-          <td>
-            <select
-              name="size"
-              key="size"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              disabled={!brand || !name || isLoading}
-            >
-              <option key="default" value="">
-                Taille
-              </option>
-              {name &&
-                brand &&
-                articleData[name]["size"].map((size) => (
-                  <option key={size} value={size}>
-                    {size}
+                  {Object.keys(articleData).map((grade) => (
+                    <option key={grade} value={grade}>
+                      {grade}
+                    </option>
+                  ))}
+                </select>
+              </td>
+              <td>
+                <select
+                  name="sex"
+                  key="sex"
+                  value={sex}
+                  onChange={(e) => setSex(e.target.value)}
+                  disabled={!grade || isLoading}
+                >
+                  <option key="default" value="">
+                    Sexe
                   </option>
-                ))}
-            </select>
-          </td>
-          <td>
-            <select
-              name="quantity"
-              key="quantity"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-              disabled={!size || isLoading}
-            >
-              <option key="default" value="">
-                Quantité
-              </option>
-              {name &&
-                brand &&
-                size &&
-                Array.from({ length: 5 }, (_, i) => (
-                  <option key={i + 1} value={i + 1}>
-                    {i + 1}
+                  {grade &&
+                    Object.keys(articleData[grade]).map((sex) => (
+                      <option key={sex} value={sex}>
+                        {sex}
+                      </option>
+                    ))}
+                </select>
+              </td>
+              <td>
+                <select
+                  name="name"
+                  key="name"
+                  value={`${brand}-${name}`}
+                  onChange={(e) => {
+                    const [brand, articleName] = e.target.value.split("-");
+                    setBrand(brand);
+                    setName(articleName);
+                  }}
+                  disabled={!sex || isLoading}
+                >
+                  <option key="default" value="">
+                    Article
                   </option>
-                ))}
-            </select>
-          </td>
-          <td id="tdAddButton">
-            <button
-              id="addArticleButton"
-              type="submit"
-              disabled={!quantity || isLoading}
-            >
-              ✓
-            </button>
-          </td>
-        </tr>
-        </tbody>
+                  {sex &&
+                    Object.keys(articleData[grade][sex]).map((brand) =>
+                      Object.keys(articleData[grade][sex][brand]).map(
+                        (articleName) => (
+                          <option
+                            key={brand + articleName}
+                            value={`${brand}-${articleName}`}
+                          >
+                            {`(${brand}) ${articleName}`}
+                          </option>
+                        )
+                      )
+                    )}
+                </select>
+              </td>
+              <td>
+                <select
+                  name="size"
+                  key="size"
+                  value={size}
+                  onChange={(e) => setSize(e.target.value)}
+                  disabled={!name || isLoading}
+                >
+                  <option key="default" value="">
+                    Taille
+                  </option>
+                  {name &&
+                    articleData[grade][sex][brand][name]["size"].map((size) => (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                </select>
+              </td>
+              <td>
+                <select
+                  name="quantity"
+                  key="quantity"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                  disabled={!size || isLoading}
+                >
+                  <option key="default" value="">
+                    Quantité
+                  </option>
+                  {name &&
+                    brand &&
+                    size &&
+                    Array.from({ length: 5 }, (_, i) => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                </select>
+              </td>
+              <td id="tdAddButton">
+                <button
+                  id="addArticleButton"
+                  type="submit"
+                  disabled={!quantity || isLoading}
+                >
+                  ✓
+                </button>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </form>
     </div>
