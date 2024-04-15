0bb5748e67712c0d9e88c4c8bb1f1451de6a8fbd
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-04-15 13:55:36 +0900

    add styles and put form to table

--- src/app/components/ArticleModifier/ArticleModifier.jsx
+++ src/app/components/ArticleModifier/ArticleModifier.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import ArticleSearch from "./ArticleSearch/ArticleSearch";
+import ArticleSearch from "../ArticleSearch/ArticleSearch";
 import { getAllClothesInfo } from "@/app/data/clothesJSONActions";
 import { updateArticle } from "./modifierAction";
+
+import "./styles.css";
 
 const ArticleModifier = () => {
   const [articleJSONData, setArticleJSONData] = useState(null);
@@ -31,96 +33,112 @@ const ArticleModifier = () => {
 
       {/* Modification Form */}
       {articleData && articleJSONData && (
-        <form onSubmit={handleSubmit}>
-          <span>
-            <label>
-              Marque
-              <select
-                name="brand"
-                key="brand"
-                value={articleData.brand}
-                onChange={(e) =>
-                  setArticleData({ ...articleData, brand: e.target.value })
-                }
-              >
-                {articleJSONData.brand.map((brand) => (
-                  <option key={brand} value={brand}>
-                    {brand}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </span>
+        <form id="modificationForm" onSubmit={handleSubmit}>
+          <table id="modificationTable">
+            <tbody>
+              <tr>
+                <td>
+                  <label htmlFor="brand">Marque : </label>
+                </td>
+                <td>
+                  <select
+                    name="brand"
+                    id="brand"
+                    className="modificationSelect"
+                    value={articleData.brand}
+                    onChange={(e) =>
+                      setArticleData({ ...articleData, brand: e.target.value })
+                    }
+                  >
+                    {articleJSONData.brand.map((brand) => (
+                      <option key={brand} value={brand}>
+                        {brand}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
 
-          <span>
-            <label>
-              Article
-              <select
-                name="name"
-                key="name"
-                value={articleData.name}
-                onChange={(e) =>
-                  setArticleData({ ...articleData, name: e.target.value })
-                }
-              >
-                {articleJSONData.name.map((name) => (
-                  <option key={name} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </span>
+              <tr>
+                <td>
+                  <label htmlFor="name">Article : </label>
+                </td>
+                <td>
+                  <select
+                    name="name"
+                    id="name"
+                    className="modificationSelect"
+                    value={articleData.name}
+                    onChange={(e) =>
+                      setArticleData({ ...articleData, name: e.target.value })
+                    }
+                  >
+                    {articleJSONData.name.map((name) => (
+                      <option key={name} value={name}>
+                        {name}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
 
-          <span>
-            <label>
-              Taille
-              <select
-                name="size"
-                key="size"
-                value={articleData.size}
-                onChange={(e) =>
-                  setArticleData({ ...articleData, size: e.target.value })
-                }
-              >
-                {articleJSONData.size.map((size) => (
-                  <option key={size} value={size}>
-                    {size}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </span>
+              <tr>
+                <td>
+                  <label htmlFor="size">Taille : </label>
+                </td>
+                <td>
+                  <select
+                    name="size"
+                    id="size"
+                    className="modificationSelect"
+                    value={articleData.size}
+                    onChange={(e) =>
+                      setArticleData({ ...articleData, size: e.target.value })
+                    }
+                  >
+                    {articleJSONData.size.map((size) => (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
 
-          <span>
-            <label>
-              État
-              <select
-                name="state"
-                key="state"
-                value={articleData.state}
-                onChange={(e) =>
-                  setArticleData({ ...articleData, state: e.target.value })
-                }
-              >
-                <option key="1" value="1">
-                  Enregistré
-                </option>
-                <option key="2" value="2">
-                  Inventorié
-                </option>
-                <option key="3" value="3">
-                  Vendu
-                </option>
-                <option key="0" value="0">
-                  Supprimé
-                </option>
-                <option key="-1" value="-1">
-                  Invendable
-                </option>
-              </select>
-            </label>
-          </span>
+              <tr>
+                <td>
+                  <label htmlFor="state">État : </label>
+                </td>
+                <td>
+                  <select
+                    name="state"
+                    id="state"
+                    className="modificationSelect"
+                    value={articleData.state}
+                    onChange={(e) =>
+                      setArticleData({ ...articleData, state: e.target.value })
+                    }
+                  >
+                    <option key="1" value="1">
+                      Enregistré
+                    </option>
+                    <option key="2" value="2">
+                      Inventorié
+                    </option>
+                    <option key="3" value="3">
+                      Vendu
+                    </option>
+                    <option key="0" value="0">
+                      Supprimé
+                    </option>
+                    <option key="-1" value="-1">
+                      Invendable
+                    </option>
+                  </select>
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
           <button type="submit" disabled={isLoading}>
             {isLoading ? "Chargement..." : "Modifier"}
