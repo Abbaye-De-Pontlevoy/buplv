f6dc325d09f006e645b9e51f22087e2af85ec449
Thibault Colcom <thibault.colcomb@hotmail.com>
2024-05-14 19:29:30 +0900

    correct error when validate with no enter

--- src/app/components/ArticleSearch/ArticleSearch.jsx
+++ src/app/components/ArticleSearch/ArticleSearch.jsx
@@ -4,49 +4,54 @@ import { getArticleByID } from "../../helpers/getArticleByID";
 import "./styles.css";
 
 const ArticleSearch = ({
-  onArticleSearch,
-  placeholder = "Rechercher par ID",
-  buttonText = "Rechercher",
+    onArticleSearch,
+    placeholder = "Rechercher par ID",
+    buttonText = "Rechercher",
 }) => {
-  // Initialize state variables
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+    // Initialize state variables
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-  // Handle the search form submission
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+    // Handle the search form submission
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
 
-    // Get the article ID from the form
-    const articleID = e.target.articleId.value;
+        // Get the article ID from the form
+        const articleID = e.target.articleId.value;
 
-    // Try to find it in the database
-    try {
-      const result = await getArticleByID(articleID);
-      if (!result) setError("Article non trouvé.");
-      // if the article is found, call the onArticleSearch function
-      await onArticleSearch(result);
-    } catch (e) {
-      setError(e.message);
-    }
+        // Try to find it in the database
+        try {
+            if (!articleID) throw new Error("Veuillez entrer un ID d'article.");
+            const result = await getArticleByID(articleID);
+            if (!result) setError("Article non trouvé.");
+            // if the article is found, call the onArticleSearch function
+            await onArticleSearch(result);
+        } catch (e) {
+            setError(e.message);
+        }
 
-    // Reset the form
-    e.target.articleId.value = "";
+        // Reset the form
+        e.target.articleId.value = "";
 
-    setIsLoading(false);
-  };
+        setIsLoading(false);
+    };
 
-  return (
-    <form onSubmit={handleSearch} id="articleSearchForm">
-      <span id="articleSearchSpan">
-        <input type="number" name="articleId" placeholder={placeholder}></input>
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Chargement..." : buttonText}
-        </button>
-      </span>
-      <p className="error">{error}</p>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSearch} id="articleSearchForm">
+            <span id="articleSearchSpan">
+                <input
+                    type="number"
+                    name="articleId"
+                    placeholder={placeholder}
+                ></input>
+                <button type="submit" disabled={isLoading}>
+                    {isLoading ? "Chargement..." : buttonText}
+                </button>
+            </span>
+            <p className="error">{error}</p>
+        </form>
+    );
 };
 
 export default ArticleSearch;
